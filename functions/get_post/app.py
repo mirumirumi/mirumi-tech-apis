d9d0e178a1f9a674d83b25a93df0142ea175a8c4
mirumi <info@milmemo.net>
2022-06-28 23:41:12 +0900

    add: fixed and added from new template repo

--- functions/get_post/app.py
+++ functions/get_post/app.py
@@ -1,22 +1,26 @@
 from __future__ import annotations
-from typing import Any, Final, NoReturn, TypedDict
+from typing import Any, cast, Literal, TypedDict
 
 import os
 import json
 import boto3
-import secret
+from proxy_response import *
 from aws_lambda_powertools.logging import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 # PowerTools
 logger = Logger()
 
-# Env Vars
+# Env vars
 ENVIRONMENT_NAME = os.environ["ENVIRONMENT_NAME"]
+
+# DynamoDB
+POST_TABLE_NAME = os.environ["POST_TABLE_NAME"]
+post_table = boto3.resource("dynamodb").Table(POST_TABLE_NAME)
 
 
 @logger.inject_lambda_context
-def lambda_handler(event: object, context: LambdaContext) -> None:
+def lambda_handler(event: dict[str, Any], context: LambdaContext) -> ProxyResponse:
     logger.info(event)
 
 
@@ -27,5 +31,4 @@ def lambda_handler(event: object, context: LambdaContext) -> None:
 
 
 
-    logger.info("✅ Succeeded!")
     return
