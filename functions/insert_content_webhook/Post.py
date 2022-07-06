145c9a4e9f1414d3ed857e21b7c906c5a95a4322
mirumi <mail@mirumi.tech>
2022-07-06 22:01:04 +0900

    add: insert-content-webhook (before custom html)

---
+++ functions/insert_content_webhook/Post.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+from typing import Any, cast, Literal, TypedDict
+
+import re
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+JST = ZoneInfo("Asia/Tokyo")
+
+
+class Post:
+    def __init__(self, slag: str, body: str) -> None:
+        self.slag = slag
+        self.title = self.front_matter_title(body)
+        self.tags = self.front_matter_tags(body)
+        self.body = self.remove_front_matter(body)
+        self.created_at = datetime.now(JST).isoformat()
+        self.updated_at = datetime.now(JST).isoformat()
+
+    @staticmethod
+    def front_matter_title(body_md: str) -> str:
+        lines = body_md.splitlines()
+        for line in lines:
+            if (line.startswith("title")):
+                return re.sub("title\s*:\s*(.*?)$", "\\1", line)  # https://regex101.com/r/4roRGw/1
+        raise Exception("front matter of `title` was not found")
+   
+    @staticmethod
+    def front_matter_tags(body_md: str) -> list[str]:
+        lines = body_md.splitlines()
+        for line in lines:
+            if (line.startswith("tags")):
+                return re.sub("tags\s*:\s*\[(.*?)\]$", "\\1", line).replace(" ", "").split(",")  # https://regex101.com/r/5Z0OqH/1
+        raise Exception("front matter of `tags` was not found")
+
+    @staticmethod
+    def remove_front_matter(body_md: str) -> str:
+        lines = body_md.splitlines()
+        is_second_bar = False
+        for i, line in enumerate(lines):
+            if line == "---" and not is_second_bar:
+                is_second_bar = True
+                continue
+            if line == "---" and is_second_bar:
+                j = 1
+                try: 
+                    while lines[i + j] == "":
+                        j += 1
+                    return "\n".join(lines[i + j:])
+                except IndexError:
+                    raise Exception("content body was not found")
+        raise Exception("front matter was not found")
--- functions/insert_content_webhook/app.py
+++ functions/insert_content_webhook/app.py
@@ -1,47 +1,81 @@
 from __future__ import annotations
-from typing import Any, cast, Literal, TypedDict
+from typing import Any, cast, Final, Literal, TypedDict
 
 import os
+import re
 import json
 import boto3
 import secret
+import base64
+import requests
+import markdown2
+from Post import Post
 from proxy_response import *
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from aws_lambda_powertools.logging import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
-# PowerTools
 logger = Logger()
 
-# DynamoDB
 POST_TABLE_NAME = os.environ["POST_TABLE_NAME"]
 post_table = boto3.resource("dynamodb").Table(POST_TABLE_NAME)
+
+JST = ZoneInfo("Asia/Tokyo")
 
 
 @logger.inject_lambda_context
 def lambda_handler(event: dict[str, Any], context: LambdaContext) -> ProxyResponse:
     logger.info(event)
+    changed_file_paths = json.loads(event["body"])["files"].split(" ")
+    logger.info(changed_file_paths)
 
+    # get contents of changed file
 
-    # 更新されたファイル一覧を取得する
+    endpoint_base = "https://api.github.com/repos/mirumirumi/mirumi-tech-content/contents/posts/"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": secret.GITHUB_PAT,
+    }
 
+    changed_posts: list[str] = list()
 
+    for changed_file_path in changed_file_paths:
+        if not "posts/" in changed_file_path:
+            continue
+        if changed_file_path == "posts/template.md":
+            continue
+        changed_posts.append(re.sub("posts\/(.*?.md)$", "\\1", changed_file_path))  # https://regex101.com/r/ZHOHDe/1
 
+    posts_to_insert :list[Post] = list()
+
+    for file_name in changed_posts:
+        res = requests.get(endpoint_base + file_name, headers=headers, timeout=(9.0, 90.0))
+
+        try:
+            res.raise_for_status()
+        except Exception as e:
+            logger.exception(str(res.status_code) + ": " + str(e))
+            return s500()
+        else:
+            body_md = base64.b64decode(json.loads(res.text)["content"].encode()).decode()
+            post = Post(slag=file_name, body=body_md)
+            posts_to_insert.append(post)
 
     # markdown to html
+    for post in posts_to_insert:
+        post.body = markdown2.markdown(post.body)
+        post.body = post.body.replace("\n", "")
+
+    # customize HTMLs
 
 
 
 
 
-    # 各HTMLのカスタマイズ
 
-
-
-
-
-
-    # テーブルに書き込む
-
+    # insert table
+        # created_atについてだけ気をつけて！
 
 
 
