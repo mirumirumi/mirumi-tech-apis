f0fc733547d95339e0bc77a769bfb955fb959cfa
mirumi <mail@mirumi.tech>
2022-07-08 12:34:54 +0900

    add: add_toc_attrs()

--- functions/insert_content_webhook/customize_html.py
+++ functions/insert_content_webhook/customize_html.py
@@ -3,6 +3,8 @@ import re
 
 def customize_html(html: str) -> str:
 
+    # add TOC attributes
+    html = add_toc_attrs(html)
 
 
 
@@ -12,8 +14,11 @@ def customize_html(html: str) -> str:
 
 
 
+def add_toc_attrs(html: str) -> str:
+    headings = re.findall("((<h[234])>(.*?)(<\/h[234]>))", html)
 
+    for i, head in enumerate(headings):
+        html = re.sub(headings[i][0], f"{head[1]} id=\"{urllib.parse.quote(head[2])}\" class=\"toc_item\" data-toc-index=\"{i + 1}\">{head[2]}{head[3]}", html)
 
+    return html
 
-
-    return
