3f10c1c215775a04fed841f2295760a0e37d9466
mirumi <mail@mirumi.tech>
2022-07-10 14:05:03 +0900

    add(customize_html): fix_img_src()

--- functions/insert_content_webhook/customize_html.py
+++ functions/insert_content_webhook/customize_html.py
@@ -16,9 +16,11 @@ def customize_html(html: str) -> str:
     # add date into rewrite box
     html = add_date_into_rewritebox(html)
 
+    # add github content domain in `img`
+    html = fix_img_src(html)
+
+
     return html
-
-
 
 
 def add_toc_attrs(html: str) -> str:
@@ -101,3 +103,11 @@ def add_date_into_rewritebox(html: str) -> str:
         html = html.replace(box[0], box[0] + "<span class=\"rewrite-date\">追記 (" + box[1] + ") ：</span>")
 
     return html
+
+
+def fix_img_src(html: str) -> str:
+    return re.sub(
+        "(<p.*?><img.*?src=\")((.*?)images\/)(.*?)(\".*?\/><\/p>)",  # https://regex101.com/r/z7iULl/1
+        "\\1https://raw.githubusercontent.com/mirumirumi/mirumi-tech-content/main/images/\\4\\5",
+        html
+    )
