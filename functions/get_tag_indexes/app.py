ed24244bde898059a0bce9eafd2a2b0212cce9db
mirumi <mail@mirumi.tech>
2022-07-12 21:49:43 +0900

    add: get-tag-indexes complete

--- functions/get_tag_indexes/app.py
+++ functions/get_tag_indexes/app.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 from typing import Any, cast, Literal, TypedDict
 
 import os
-import json
 import boto3
 from constants import *
 from proxy_response import *
+from boto3.dynamodb.conditions import Attr
 from aws_lambda_powertools.logging import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
@@ -18,13 +18,22 @@ post_table = boto3.resource("dynamodb").Table(POST_TABLE_NAME)
 @logger.inject_lambda_context
 def lambda_handler(event: dict[str, Any], context: LambdaContext) -> ProxyResponse:
     logger.info(event)
+    page = int(event["queryStringParameters"]["page"])
+    tag = event["queryStringParameters"]["tag"]
 
+    result = None
+    try:
+        res = post_table.scan(
+            FilterExpression=Attr("tags").contains(tag),
+            ProjectionExpression="slag, title, created_at, updated_at",
+        )
+        result = res["Items"]
+    except Exception as e:
+        logger.exception(e)
+        return s500()
 
+    result.sort(key=lambda x: cast(str, x["created_at"]), reverse=True)        
 
+    result = result[(page - 1) * PAGE_ITEMS : page * PAGE_ITEMS]
 
-
-
-
-
-
-    return
+    return s200(result)
