7a28252bbfee234f99bdcf16a95794f798ab576b
mirumi <mail@mirumi.tech>
2022-07-15 17:35:29 +0900

    add: api allow origin miscs

--- functions/layers/utils_layer/proxy_response.py
+++ functions/layers/utils_layer/proxy_response.py
@@ -1,17 +1,24 @@
 from __future__ import annotations
-from typing import Any, cast, Literal, TypedDict
+from typing import Any, cast, Literal, Optional, TypedDict
 
+import os
 import json
 import dynamodb
+
+ALLOWED_CLIENT_ORIGIN = os.getenv("ALLOWED_CLIENT_ORIGIN")
 
 
 class ProxyResponse(TypedDict):
     statusCode: int
+    headers: dict[str, str | None]
     body: str
 
 def s200(val: object = None) -> ProxyResponse:
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": ALLOWED_CLIENT_ORIGIN,
+        },
         "body": json.dumps(
             val if val is not None else "no response data",
             default=dynamodb.decimal_to_float,
@@ -21,18 +28,27 @@ def s200(val: object = None) -> ProxyResponse:
 def s400() -> ProxyResponse:
     return {
         "statusCode": 400,
+        "headers": {
+            "Access-Control-Allow-Origin": ALLOWED_CLIENT_ORIGIN,
+        },
         "body": "required request data is missing or I/F schema is invalid",
     }
 
 def s403() -> ProxyResponse:
     return {
         "statusCode": 403,
-        "body": "required api key is missing or invalid",
+        "headers": {
+            "Access-Control-Allow-Origin": ALLOWED_CLIENT_ORIGIN,
+        },
+        "body": "required api key is missing or invalid, alternatively, your referrer is not allowed",
     }
 
 def s500(val: object = None) -> ProxyResponse:
     return {
         "statusCode": 500,
+        "headers": {
+            "Access-Control-Allow-Origin": ALLOWED_CLIENT_ORIGIN,
+        },
         "body": json.dumps(
             val if val is not None else "no response data",
             default=dynamodb.decimal_to_float,
