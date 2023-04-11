8d4166f929f55f168be75db312493db0cb46516c
mirumi <mail@mirumi.tech>
2023-04-11 23:57:46 +0900

    fix: dynamodb.rs

--- src/bin/utils/dynamodb.rs
+++ src/bin/utils/dynamodb.rs
@@ -8,6 +8,24 @@ use std::collections::HashMap;
 
 pub struct AttributeValueMap(pub HashMap<String, AttributeValue>);
 
+impl AttributeValueMap {
+    fn serialize_attribute_value(value: &AttributeValue) -> serde_json::Value {
+        match value {
+            AttributeValue::S(s) => json!(s),
+            AttributeValue::N(n) => json!(n),
+            AttributeValue::Bool(b) => json!(b),
+            AttributeValue::Null(_) => json!(null),
+            AttributeValue::Ss(ss) => json!(ss),
+            AttributeValue::Ns(ns) => json!(ns),
+            AttributeValue::L(l) => json!(l
+                .iter()
+                .map(|item| Self::serialize_attribute_value(item))
+                .collect::<Vec<_>>()),
+            _ => unimplemented!("No serializer for DynamoDB AttributeValue."),
+        }
+    }
+}
+
 impl Serialize for AttributeValueMap {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -16,26 +34,7 @@ impl Serialize for AttributeValueMap {
         let mut map = serializer.serialize_map(Some(self.0.len()))?;
 
         for (key, value) in &self.0 {
-            let serialized = match value {
-                AttributeValue::S(s) => json!(s),
-                AttributeValue::N(n) => json!(n),
-                AttributeValue::Bool(b) => json!(b),
-                AttributeValue::Null(_) => json!(null),
-                AttributeValue::Ss(ss) => json!(ss),
-                AttributeValue::Ns(ns) => json!(ns),
-                AttributeValue::L(l) => json!(l
-                    .iter()
-                    .map(|item| {
-                        serde_json::to_value(AttributeValueMap(
-                            vec![("item".to_string(), item.clone())]
-                                .into_iter()
-                                .collect(),
-                        ))
-                        .unwrap()
-                    })
-                    .collect::<Vec<serde_json::Value>>()),
-                _ => unimplemented!("No serializer for DynamoDB AttributeValue."),
-            };
+            let serialized = Self::serialize_attribute_value(value);
             map.serialize_entry(key, &serialized)?;
         }
 
